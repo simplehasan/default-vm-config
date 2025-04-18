de4a03840daf63781e232c90b67fe9e616c62ddd
abd <136981949+simplehasan@users.noreply.github.com>
2025-04-18 16:19:26 +1000

    Update docker.sh
    fix error

--- docker.sh
+++ docker.sh
@@ -18,7 +18,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt update
 
 # 6 Finally, install Docker:
-sudo apt update && sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo usermod -aG docker ${USER}
 
 # 7 Install cri-dockerd
