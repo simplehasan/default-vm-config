6fd2e043bf47ab506a7b72a9a3de415a284fa6ca
abd <136981949+simplehasan@users.noreply.github.com>
2025-04-18 16:25:41 +1000

    Update iptables.sh
    echo every rule

--- iptables.sh
+++ iptables.sh
@@ -3,18 +3,23 @@
 # Add Kubernetes-related TCP/UDP rules
 
 # Kubernetes API Server port
+echo "adding port 6443"
 sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 6443 -j ACCEPT
 
 # etcd ports
+echo "adding port 2379,2380"
 sudo iptables -A INPUT -p tcp -m state --state NEW --match multiport --dport 2379,2380 -j ACCEPT
 
 # Kubelet, proxy and other Kubernetes services ports
+echo "adding port 10250,10256,10257,10259"
 sudo iptables -A INPUT -p tcp -m state --state NEW --match multiport --dport 10250,10256,10257,10259 -j ACCEPT
 
 # NodePort range for Kubernetes services
+echo "adding port 30000:32767"
 sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 30000:32767 -j ACCEPT
 
 # Consul or other services
+echo "adding port 6783"
 sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 6783 -j ACCEPT
 sudo iptables -A INPUT -p udp -m udp --dport 6783 -j ACCEPT
 sudo iptables -A INPUT -p udp -m udp --dport 6784 -j ACCEPT
